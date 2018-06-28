972761cba1dc9be1046deffc385cc2a015c750a8
mattia mertens <mattianc@hotmail.it>
2018-06-28 11:15:17 +0200

    immagini

--- fewo.js
+++ fewo.js
@@ -71,7 +71,7 @@ function sldshow() {
   $(".box").toggleClass("novis");
   $(".copritutto").toggleClass("visibile");
   $(this).toggleClass("sldshow")
-};
+}; //attivare lo slideshow fasullo
 function back(){
     $(".box").removeClass("novis");
     $(".copritutto").removeClass("visibile");
@@ -79,3 +79,13 @@ function back(){
 };
 $(".copritutto").click(back); //funzione per togliere lo slideshow aggiuntiva
 $(".box1, .box2, .box3, .box4, .box5, .box6, .box7").click(sldshow);
+
+function attivita() {
+  $(this).animate({
+    left: "0px",
+    width: "+=900px",
+  });
+  $(".coverdiv").addClass("hovered")
+}
+
+$(".ac1, .ac2, .ac3, .ac4, .ac5").click(attivita)
